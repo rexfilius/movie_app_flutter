0b4b548a1dc97c751c3f7fdb9ad14e770ec38aad
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2022-03-23 15:58:57 +0100

    Add functions for CRUD operations in database

--- lib/app/data/database/hivedb/hive_database.dart
+++ lib/app/data/database/hivedb/hive_database.dart
@@ -1,7 +1,26 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movie_app_flutter/movie_app_lib.dart';
 
 class HiveDatabase {
   static const String name = 'movie_database';
+
+  Box<Movie> getMovieBox() => Hive.box(HiveDatabase.name);
+
+  void saveMovie(Movie movie) {
+    final box = getMovieBox();
+    box.add(movie);
+  }
+
+  List<Movie> getMovies() {
+    final box = getMovieBox();
+    final movieList = box.values.toList().cast<Movie>();
+    return movieList;
+  }
+
+  void deleteMovie(int movieIndex) {
+    final box = getMovieBox();
+    box.deleteAt(movieIndex);
+  }
 }
 
 Movie convertJsonResultToMovie(Result result) {
