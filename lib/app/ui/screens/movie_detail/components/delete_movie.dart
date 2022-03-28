c3ef792dab48699a1ecc9d607ce3323151696d3e
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2022-03-28 16:27:03 +0100

    refactor code to save and delete movie

--- lib/app/ui/screens/movie_detail/components/delete_movie.dart
+++ lib/app/ui/screens/movie_detail/components/delete_movie.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app_flutter/movie_app_lib.dart';
 
-class DeleteMovie extends StatefulWidget {
+class DeleteMovie extends StatelessWidget {
   const DeleteMovie({
     required this.result,
     Key? key,
@@ -10,42 +10,29 @@ class DeleteMovie extends StatefulWidget {
   final Result result;
 
   @override
-  State<DeleteMovie> createState() => _DeleteMovieState();
-}
-
-class _DeleteMovieState extends State<DeleteMovie> {
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Movie>(
-      future: convertJsonResultToMovie(widget.result),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return IconButton(
-            onPressed: () {
-              if (HiveDatabase().isMovieInDatabase(snapshot.data!)) {
-                HiveDatabase().removeFromFavorites(snapshot.data!);
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content:
-                      Text('${widget.result.title} removed from favorites'),
-                ));
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      '${widget.result.title} already removed from favorites',
-                    ),
-                  ),
-                );
-              }
-            },
-            icon: const Icon(
-              Icons.delete,
-              color: AppColors.white,
+    return IconButton(
+      onPressed: () async {
+        final movie = await convertJsonResultToMovie(result);
+        if (HiveDatabase().isMovieInDatabase(movie)) {
+          HiveDatabase().removeFromFavorites(movie);
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('${result.title} removed from favorites'),
+          ));
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                '${result.title} already removed from favorites',
+              ),
             ),
           );
         }
-        return const Icon(Icons.http);
       },
+      icon: const Icon(
+        Icons.delete,
+        color: AppColors.white,
+      ),
     );
   }
 }
--- lib/app/ui/screens/movie_detail/components/save_movie.dart
+++ lib/app/ui/screens/movie_detail/components/save_movie.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app_flutter/movie_app_lib.dart';
 
-class SaveMovie extends StatefulWidget {
+class SaveMovie extends StatelessWidget {
   const SaveMovie({
     required this.result,
     Key? key,
@@ -10,41 +10,27 @@ class SaveMovie extends StatefulWidget {
   final Result result;
 
   @override
-  State<SaveMovie> createState() => _SaveMovieState();
-}
-
-class _SaveMovieState extends State<SaveMovie> {
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Movie>(
-      future: convertJsonResultToMovie(widget.result),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return IconButton(
-            onPressed: () {
-              if (HiveDatabase().isMovieInDatabase(snapshot.data!)) {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text('${widget.result.title} already added'),
-                ));
-              } else {
-                HiveDatabase().addToFavorites(snapshot.data!);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      '${widget.result.title} added to favorites',
-                    ),
-                  ),
-                );
-              }
-            },
-            icon: const Icon(
-              Icons.save,
-              color: AppColors.white,
+    return IconButton(
+      onPressed: () async {
+        final movie = await convertJsonResultToMovie(result);
+        if (HiveDatabase().isMovieInDatabase(movie)) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('${result.title} already added'),
+          ));
+        } else {
+          HiveDatabase().addToFavorites(movie);
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('${result.title} added to favorites'),
             ),
           );
         }
-        return const Icon(Icons.http);
       },
+      icon: const Icon(
+        Icons.save,
+        color: AppColors.white,
+      ),
     );
   }
 }
